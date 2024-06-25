298f77cbb2b97ebdb6fe17e94dfe7c27f03deaaf
pablogg411 <pablogg411@gmail.com>
2024-06-25 17:24:39 +0200

    Made showcase selection bar responsive and added language support

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -5,7 +5,7 @@ const portfolio = await getCollection("portfolio");
 const productTypes = ["App", "Website", "Product", "Web App", "SaaS"];
 const businessTypes = ["Business", "Startup"];
 
-export default function Showcase() {
+export default function Showcase({ lang = "en" }: { lang: "es" | "en" }) {
   const [tab, setTab] = useState("Favourites");
 
   function selectTab(nextTab: string) {
@@ -14,30 +14,30 @@ export default function Showcase() {
 
   return (
     <>
-      <div className="fixed left-1/2 top-[80px] z-10 flex w-fit -translate-x-1/2 -translate-y-1/2 gap-2 rounded-md bg-[var(--content)] p-2 text-[var(--content-foreground)] backdrop-blur-[30px]">
+      <div className="fixed left-1/2 top-[80px] z-10 flex -translate-x-1/2 -translate-y-1/2 gap-2 rounded-md bg-[var(--content)] p-2 text-[var(--content-foreground)] backdrop-blur-[30px] w-[90%] sm:w-fit max-w-fit overflow-auto">
         <button
           onClick={() => selectTab("Favourites")}
           className={`rounded-md px-2 py-0.5 transition-all duration-500 ${tab == "Favourites" && "bg-sky-400 text-black"}`}
         >
-          Favourites
+          {lang == "en" ? "Favourites" : "Favoritos"}
         </button>
         <button
           onClick={() => selectTab("All")}
           className={`rounded-md px-2 py-0.5 transition-all duration-500 ${tab == "All" && "bg-sky-400 text-black"}`}
         >
-          All
+          {lang == "en" ? "All" : "Todo"}
         </button>
         <button
           onClick={() => selectTab("Products")}
           className={`rounded-md px-2 py-0.5 transition-all duration-500 ${tab == "Products" && "bg-sky-400 text-black"}`}
         >
-          Products
+          {lang == "en" ? "Products" : "Productos"}
         </button>
         <button
           onClick={() => selectTab("Businesses")}
           className={`rounded-md px-2 py-0.5 transition-all duration-500 ${tab == "Businesses" && "bg-sky-400 text-black"}`}
         >
-          Businesses
+          {lang == "en" ? "Businesses" : "Empresas"}
         </button>
       </div>
       <div className="grid w-full max-w-96 grid-flow-dense grid-cols-1 gap-4 px-6 pt-12 md:max-w-3xl md:grid-cols-2 lg:max-w-6xl lg:grid-cols-3">
