f8c8e545131f1862bc04597bf0b0d70310c98c71
pablogg411 <pablogg411@gmail.com>
2024-06-30 11:12:16 +0200

    Small improvements

--- src/content/data.ts
+++ src/content/data.ts
@@ -1,78 +1,78 @@
 export const experience = [
-	{
-		company: 'Qosit Consulting',
-		position: 'Intern'
-	},
-	{
-		company: "It'sFree",
-		position: 'Chief Operating Officer'
-	},
-	{
-		company: 'Omnia',
-		position: 'Co-Chief Executive Officer'
-	}
-]
+  {
+    company: "Qosit Consulting",
+    position: "Intern",
+  },
+  {
+    company: "It'sFree",
+    position: "Chief Operating Officer",
+  },
+  {
+    company: "Omnia",
+    position: "Co-Chief Executive Officer",
+  },
+];
 
 export const inspiration = [
-	{
-		name: 'Ali Abdaal',
-		desc: 'Ex-Doctor and YouTuber focused on productivity and enjoying the process of things.'
-	},
-	{
-		name: 'Tiago Forte',
-		desc: 'Creator of the PARA framework to organise our thoughts and ideas.'
-	}
-]
+  {
+    name: "Ali Abdaal",
+    desc: "Ex-Doctor and YouTuber focused on productivity and enjoying the process of things.",
+  },
+  {
+    name: "Tiago Forte",
+    desc: "Creator of the PARA framework to organise our thoughts and ideas.",
+  },
+];
 
 export const socials = [
-	{
-		name: 'LinkedIn',
-		text: '@pabgamgar',
-		url: 'https://linkedin.com/in/pabgamgar',
-		icon: 'linkedin-svgl.webp'
-	},
-	{
-		name: 'Instagram',
-		text: '@pabgamgar',
-		url: 'https://www.instagram.com/pabgamgar',
-		icon: 'instagram-svgl.webp'
-	},
-	{
-		name: 'Github',
-		text: '@pabgamgar',
-		url: 'https://www.github.com/pabgamgar',
-		icon: 'github-svgl.webp'
-	},
-	{
-		name: 'Twitter',
-		text: '@pabgamgar',
-		url: 'https://twitter.com/pabgamgar',
-		icon: 'twitter-svgl.webp'
-	}
-]
+  {
+    name: "LinkedIn",
+    text: "@pabgamgar",
+    url: "https://linkedin.com/in/pabgamgar",
+    icon: "linkedin-svgl.webp",
+  },
+  {
+    name: "Instagram",
+    text: "@pabgamgar",
+    url: "https://www.instagram.com/pabgamgar",
+    icon: "instagram-svgl.webp",
+  },
+  {
+    name: "Github",
+    text: "@pabgamgar",
+    url: "https://www.github.com/pabgamgar",
+    icon: "github-svgl.webp",
+  },
+  {
+    name: "Twitter",
+    text: "@pabgamgar",
+    url: "https://twitter.com/pabgamgar",
+    icon: "twitter-svgl.webp",
+  },
+];
 
 export const linktreeProjectsBusinesses = [
-	{
-		name: 'Day Keeper',
-		text: 'App',
-		url: 'https://daykeeper.pablogamero.com',
-		icon: 'daykeeper-icon.webp'
-	},
-	{
-		name: 'Omnia',
-		text: 'StartUp',
-		url: 'https://www.omniacorp.es',
-		icon: 'omnia-icon.webp'
-	},
-	{
-		name: "It'sFree",
-		text: 'Business',
-		url: 'https://www.itsfree-web.web.app',
-		icon: 'itsfree-icon.webp'
-	},
-	{
-		name: 'Research Tool',
-		text: 'Web App',
-		url: 'https://researchtool.pablogamero.com'
-	}
-]
+  {
+    name: "Day Keeper",
+    text: "App",
+    url: "https://daykeeper.pablogamero.com",
+    icon: "daykeeper-icon.webp",
+  },
+  {
+    name: "Omnia",
+    text: "StartUp",
+    url: "https://www.omniacorp.es",
+    icon: "omnia-icon.webp",
+  },
+  {
+    name: "It'sFree",
+    text: "Business",
+    url: "https://www.itsfree-web.web.app",
+    icon: "itsfree-icon.webp",
+  },
+  {
+    name: "Research Tool",
+    text: "Web App",
+    url: "https://researchtool.pablogamero.com",
+  },
+];
