f31d81b71597e390bc646fa22d708de95b402b76
pabgamgar <blabligamerito@gmail.com>
2024-11-16 20:45:51 +0100

    better project images, changed flag emojis, made project pages readable

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,26 @@
 import { z, defineCollection } from "astro:content";
 
-const portfolioCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    name: z.string(),
-    link: z.string(),
-    logo: z.string(),
-    type: z.string(),
-    description: z.array(z.string()),
-    sector: z.optional(z.array(z.array(z.string()))),
-    technologies: z.optional(z.array(z.string())),
-    socialMedia: z.optional(z.array(z.array(z.string()))),
-    pinned: z.optional(z.boolean()),
-  }),
+export const PortfolioItemSchema = z.object({
+	name: z.string(),
+	link: z.string(),
+	logo: z.string(),
+	type: z.string(),
+	description: z.array(z.string()),
+	sector: z.optional(z.array(z.array(z.string()))),
+	technologies: z.optional(z.array(z.string())),
+	socialMedia: z.optional(z.array(z.array(z.string()))),
+	pinned: z.optional(z.boolean()),
+	slug: z.string(),
+	body: z.string(),
 });
 
+const portfolioCollection = defineCollection({
+	type: "content",
+	schema: PortfolioItemSchema.omit({ slug: true, body: true }),
+});
+
+export type PortfolioItem = z.infer<typeof PortfolioItemSchema>;
+
 export const collections = {
-  portfolio: portfolioCollection,
+	portfolio: portfolioCollection,
 };
